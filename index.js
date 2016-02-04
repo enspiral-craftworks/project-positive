dca47e72a29d5a141314399f7506530ca205c1bc
Sarah Rogers <srogersnz@gmail.com>
2016-02-05 12:13:30 +1300

    Get facebook feed showing on page

--- index.js
+++ index.js
@@ -1,20 +1,16 @@
+// FACEBOOK JS
+window.fbAsyncInit = function() {
+  FB.init({
+    appId      : '1761799850709452',
+    xfbml      : true,
+    version    : 'v2.5'
+  });
+};
 
-// Facebook feed
-// Facebook SDK code
-
-// window.fbAsyncInit = function() {
-//   FB.init({
-//     appId      : '1761799850709452',
-//     xfbml      : true,
-//     version    : 'v2.5'
-//   })
-// }
-
-// function(d, s, id) {
-//   var js, fjs = d.getElementsByTagName(s)[0]
-//   if (d.getElementById(id)) return
-//   js = d.createElement(s)
-//   js.id = id
-//   js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.5"
-//   fjs.parentNode.insertBefore(js, fjs)
-// } (document, 'script', 'facebook-jssdk')
+(function(d, s, id){
+   var js, fjs = d.getElementsByTagName(s)[0];
+   if (d.getElementById(id)) {return;}
+   js = d.createElement(s); js.id = id;
+   js.src = "//connect.facebook.net/en_US/sdk.js";
+   fjs.parentNode.insertBefore(js, fjs);
+ }(document, 'script', 'facebook-jssdk'));
