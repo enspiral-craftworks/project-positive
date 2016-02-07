0f3c535c0ef8b346eef58ae4e5186c0b30bb3ad5
Sarah Rogers <srogersnz@gmail.com>
2016-02-08 10:48:14 +1300

    Update twitter feed to display hashtags instead of user profile

--- assets/js/index.js
+++ assets/js/index.js
@@ -18,13 +18,13 @@ window.fbAsyncInit = function() {
 // TWITTER JS
 !function(d,s,id) {
   var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https'
-  if(!d.getElementById(id)){js=d.createElement(s)
-  js.id=id
-  js.src=p+"://platform.twitter.com/widgets.js"
-  fjs.parentNode.insertBefore(js,fjs)
+  if(!d.getElementById(id)){
+    js=d.createElement(s)
+    js.id=id
+    js.src=p+"://platform.twitter.com/widgets.js"
+    fjs.parentNode.insertBefore(js,fjs)
   }
 } (document,"script","twitter-wjs");
-
 
 // INSTAGRAM JS
 
